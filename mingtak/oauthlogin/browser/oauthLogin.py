135434da53ee87a5923cdb7244d1f6a0f8b0bb6f
andy fang <andy@mingtak.com.tw>
2015-02-14 12:14:46 +0000

    modified, notify userinitiallogin

--- mingtak/oauthlogin/browser/oauthLogin.py
+++ mingtak/oauthlogin/browser/oauthLogin.py
@@ -1,6 +1,7 @@
 from Products.Five.browser import BrowserView
 import logging
 from plone import api
+from DateTime import DateTime
 from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 import urllib2
@@ -10,7 +11,7 @@ from Products.CMFPlone.utils import safe_unicode
 from oauthlib.oauth2 import TokenExpiredError
 import os; os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 from zope.event import notify
-from Products.PlonePAS.events import UserLoggedInEvent
+from Products.PlonePAS.events import UserLoggedInEvent, UserInitialLoginInEvent
 
 logger = logging.getLogger("mingtak.oauthlogin.browser.oauthLogin")
 
@@ -38,8 +39,8 @@ class OauthWorkFlow(object):
         return getUser
 
     def createUser(self, userid, email, properties):
-        api.user.create(username=userid, email=email, properties=properties,)
-        return
+        userObject = api.user.create(username=userid, email=email, properties=properties,)
+        return userObject
 
 
 class FacebookLogin(BrowserView):
@@ -69,6 +70,11 @@ class FacebookLogin(BrowserView):
             self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
             self.request.RESPONSE.redirect("/")
             # event handle, fired to UserLoggedInEvent
+#            default = DateTime('2000/01/01')
+#            login_time = userObject.getProperty('login_time', default)
+#            if login_time.strftime("%Y") == default.strftime("%Y"):
+#                notify(UserInitialLoginInEvent(userObject))
+#            else:
             notify(UserLoggedInEvent(userObject))
             return
         userInfo = dict(
@@ -78,11 +84,15 @@ class FacebookLogin(BrowserView):
             fbGender=safe_unicode(user.get("gender", "")),
             home_page=safe_unicode(user.get("link", "")),
         )
-        oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
+        userObject = oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
         self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
         self.request.RESPONSE.redirect("/")
         # event handle, fired to UserLoggedInEvent
-        notify(UserLoggedInEvent(userObject))
+        ## user initial login event notify , not yat complete.
+#        default = DateTime('2000/01/01')
+#        login_time = userObject.getProperty('login_time', default)
+#        notify(UserLoggedInEvent(userObject))
+        notify(UserInitialLoginInEvent(userObject))
         return
 
 
